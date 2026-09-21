dee73144e10008061c86f35a308481185dad5774
hinag6shi <mrxyzi2007@gmail.com>
2026-09-21 22:41:33 +0500

    task 1: add command help and command registry.

--- src/main/java/ru/himukai/platform/telegram/core/Command.java
+++ src/main/java/ru/himukai/platform/telegram/core/Command.java
@@ -1,4 +1,15 @@
 package ru.himukai.platform.telegram.core;
 
+import java.util.List;
+
 public interface Command {
+    String name();
+
+    String description();
+
+    default String usage() {
+        return "/" + name();
+    }
+
+    String execute(List<String> args);
 }
